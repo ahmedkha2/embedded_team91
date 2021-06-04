727e92accf4129ab3b176ce421d7f3874f53f47b
ahmedkha2 <akmr1212@gmail.com>
2021-06-04 02:35:23 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,36 @@
+#include "stdint.h"
+#include "C:/Keil/Labware/inc/tm4c123gh6pm.h"
+void systeminit (){}
+void init(){
+	uint32_t delay;
+	SYSCTL_RCGCGPIO_R |= 0x20;
+	delay=1;
+	GPIO_PORTF_LOCK_R = 0x4C4F434B;
+	GPIO_PORTF_CR_R = 0X1F;
+	GPIO_PORTF_DIR_R = 0x0E;
+	GPIO_PORTF_AFSEL_R = 0x0;
+	GPIO_PORTF_PCTL_R = 0x0;
+	GPIO_PORTF_AMSEL_R = 0x0;
+	GPIO_PORTF_DEN_R = 0X1F;
+	GPIO_PORTF_PUR_R = 0X11;
+	
+
+}
+int main(){
+	init();
+	while(1)
+	{
+	if((GPIO_PORTF_DATA_R&0x11)==0x01){
+		GPIO_PORTF_DATA_R = 0x08;
+	}
+	else if((GPIO_PORTF_DATA_R&0x11)==0x10){
+		GPIO_PORTF_DATA_R = 0x04;
+	}
+	else if((GPIO_PORTF_DATA_R&0x11)==0x0){
+		GPIO_PORTF_DATA_R = 0x02;
+	}
+	else{
+		GPIO_PORTF_DATA_R = 0x11;
+	}
+}
+}
